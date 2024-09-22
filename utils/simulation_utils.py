160d392c15630d7d46ba220bcd771fd25eaf3ae3
Malte Weber <malte.weber@uni-jena.de>
2024-09-22 08:55:58 +0200

    add test functions for simulation to simulation utils

--- utils/simulation_utils.py
+++ utils/simulation_utils.py
@@ -1,6 +1,12 @@
 from ldpc import LDPC, InputType
 from channels.binary_symmetric_channel import BinarySymmetricChannel
 import numpy as np
+from typing import Callable
+from utils.graph_utils import create_tanner_graph
+from utils.ldpc_utils import create_parity_check_matrix
+import networkx as nx
+
+
 
 def run_binary_channel_simulation(
         code: LDPC,
@@ -35,3 +41,61 @@ def run_binary_channel_simulation(
     error_rate = 0 if len(bit_error_rates) == 0 else sum(bit_error_rates) / len(bit_error_rates)
 
     return convergence_rate, error_rate
+
+
+def test_girth_metric_binary_symmetric(
+        girth_metric: Callable[[nx.Graph], float],
+        nr_of_matrices: int,
+        n: int,
+        w_r: int,
+        w_c: int,
+        nr_of_transmissions: int,
+        bp_iterations: int,
+        flip_rates) -> (list[float], list[float]):
+    H, g, girths = choose_from_random_by_girth_metric(girth_metric, nr_of_matrices, n, w_r, w_c)
+
+    code = LDPC(H)
+
+    convergence_rates = []
+    bit_error_rates = []
+
+    for f in flip_rates:
+        c_rate, e_rate = run_binary_channel_simulation(
+            code,
+            f,
+            nr_of_transmissions,
+            bp_iterations
+        )
+
+        convergence_rates.append(c_rate)
+        bit_error_rates.append(e_rate)
+    return convergence_rates, bit_error_rates
+
+
+def choose_from_random_by_girth_metric(
+        girth_metric: Callable[[nx.Graph], float],
+        nr_of_matrices: int,
+        n: int,
+        w_r: int,
+        w_c: int) -> (np.ndarray, float, list[float]):
+    """
+    :param girth_metric: A function that takes a Tanner graph and returns a float
+    :param nr_of_matrices: The number of matrices to generate to choose the best from regarding the girth metric
+    :param n: The number of columns in the parity check matrix
+    :param w_r: The row weight of the parity check matrix
+    :param w_c:
+    :return:
+    """
+    best = (None, 0)
+    girths = []
+
+    for i in range(nr_of_matrices):
+        H = create_parity_check_matrix(n, w_r, w_c)
+        if H is not None:
+            g = girth_metric(create_tanner_graph(H))
+            girths.append(g)
+
+            if g > best[1]:
+                best = H, g
+
+    return best[0], best[1], girths
